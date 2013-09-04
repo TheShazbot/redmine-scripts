a9c9f43face3a3f62df6f9c6016e7d4040d6c974
William Van Hevelingen <blkperl@cat.pdx.edu>
2013-09-04 11:58:28 -0700

    Add support for multiple databases

--- cat/db.pm
+++ cat/db.pm
@@ -11,15 +11,17 @@ use base qw(Exporter);
 our (@EXPORT_OK) = qw( 'connectToDb' );
 
 sub connectToDb {
+  my ($name) = @_;
 
   my $config   = LoadFile('config.yaml');
-  my $host     = $config->{'host'};
-  my $dbname   = $config->{'dbname'};
-  my $port     = $config->{'port'};
-  my $username = $config->{'username'};
-  my $password = $config->{'password'};
+  my $host     = $config->{"$name"}{'host'};
+  my $dbname   = $config->{"$name"}{'dbname'};
+  my $port     = $config->{"$name"}{'port'};
+  my $username = $config->{"$name"}{'username'};
+  my $password = $config->{"$name"}{'password'};
+  my $driver   = $config->{"$name"}{'driver'};
 
-  my ($dbh) = DBI->connect("dbi:Pg:dbname=$dbname;host=$host;port=$port;",
+  my ($dbh) = DBI->connect("dbi:$driver:dbname=$dbname;host=$host;port=$port;",
       "$username", "$password");
 
   if ( !$dbh ) {
