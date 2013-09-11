2dd2de229301e4c78011dbe41276945274d61241
John Jendro <johnj@cat.pdx.edu>
2013-09-11 11:52:45 -0700

    Add cat::log module has functions log and errorlog

---
+++ cat/log.pm
@@ -0,0 +1,66 @@
+#!/usr/bin/env perl
+
+package cat::log;
+
+use strict;
+use DBI;
+use YAML qw(LoadFile);
+use Exporter;
+use base qw(Exporter);
+
+use cat::db;
+
+our (@EXPORT_OK) = qw( 'log' 'errorlog' );
+
+sub errorlog 
+    {
+    my ($source, $message) = @_;
+
+    my $config   = LoadFile('config.yaml');
+    my $logfile  = $config->{'errorlog'};
+
+    my($fh);
+
+    if ( open($fh, '>>', $logfile) )
+	{
+	printf("%s %10s %s\n",
+	    time2str( '%Y-%m-%d %H:%M', time() ), $source, $message);
+	}
+    else
+	{
+	print "Could not open log file\n";
+	}
+    }
+
+sub log 
+    {
+    my ($source, $message) = @_;
+
+    my $config   = LoadFile('config.yaml');
+    my $logfile  = $config->{'logfile'};
+
+    my($fh);
+
+    if ( open($fh, '>>', $logfile) )
+	{
+	printf("%s %10s %s\n",
+	    time2str( '%Y-%m-%d %H:%M', time() ), $source, $message);
+	}
+    else
+	{
+	print "Could not open log file\n";
+	}
+
+    my $dbh = cat::db::connectToDb('gitolite');
+
+    my $sth = $dbh->prepare('INSERT INTO log ( source, message) 
+	VALUES ( ?, ?)');
+
+    $sth->execute($source, $message);
+
+    $dbh->disconnect;
+
+    }
+
+
+1;
